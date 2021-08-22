7934f2b66cb2849969786ae93a01b489016fb8f3
Daniel <daniel@pink-taco.local>
2021-08-22 13:48:58 -0400

    forms and user accounts

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.urls import reverse #new
 
 # Create your models here.
 
-# many to one relationship, this means automatically that a given user can be the autohr of many different  blog post but not the other way around
+# many to one relationship, this means automatically that a given user can be the author of many different  blog post but not the other way around
 # the refernece is to the built-in User model that Django provides for authentication
 #For all many-to-one relationships such as a ForeignKey we must also specify on on_delete option 
 class Post(models.Model):
@@ -15,3 +16,15 @@ class Post(models.Model):
 
   def __str__(self):
     return self.title
+
+  def get_absolute_url(self):
+    return reverse('post_detail', args=[str(self.id)])
+
+
+
+# Reverse provides us to reference an object by its URL template name, in this case post_detail, if you recall, our URL pattern is the following:
+
+# That means in order for this route to work we must also pass in an argument with the pk(primary key) of the object
+# pk and id are interchangable in Django though the Dnango cods recommned using self.id with get_absolute_url
+#So we're telling Django that the ultimate location of a Post entry is its post_detail view which is posts /<int:pk>/ so the route for the first entry we've made will be at posts/1
+
--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.http import response
 from django.test import TestCase
-from django.test import reverse
+from django.urls import reverse
 
 from .models import Post
 # Create your tests here.
@@ -33,7 +33,7 @@ class BlogTests(TestCase):
   def test_post_list_view(self):
       response = self.client.get(reverse('home'))
       self.assertEqual(response.status_code, 200)
-      self.assertContains(response, 'Nice bode content')
+      self.assertContains(response, 'Nice body content')
       self.assertTemplateUsed(response, 'home.html')
 
   def test_post_detail_view(self):
@@ -43,3 +43,25 @@ class BlogTests(TestCase):
       self.assertEqual(no_response.status_code, 404)
       self.assertContains(response, 'A good title')
       self.assertTemplateUsed(response, 'post_detail.html')
+  
+  def test_post_create_view(self): #new
+    response = self.client.post(reverse('post_new'), {
+      'title': 'New title',
+      'body': 'New text',
+      'author': self.user.id,
+    })
+    self.assertEqual(response.status_code, 302)
+    self.assertEqual(Post.objects.last().title, 'New title')
+    self.assertEqual(Post.objects.last().body, 'New text')
+
+  def test_post_update_view(self):
+    response = self.client.post(reverse('post_edit', args='1'), {
+      'title': 'Update title',
+      'body': 'Update text',
+    })
+    self.assertEqual(response.status_code, 302)
+  
+  def test_post_delete_view(self): # new
+    response = self.client.post(
+        reverse('post_delete', args='1'))
+    self.assertEqual(response.status_code, 302)
